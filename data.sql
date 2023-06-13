1057fafb5f87b435df5380da3ba38dc0f7a7500c
Oscar Villagran <oscar.villagran@hotmail.com>
2023-06-12 22:49:47 -0500

    Add data.sql info

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon', '02-03-20', 0, TRUE, 10.23), 
+    ('Gabumon', '11-15-18', 2, TRUE, 8), 
+    ('Pikachu', '01-07-21', 1, FALSE, 15.04), 
+    ('Devimon', '05-12-17', 5, TRUE, 11);
