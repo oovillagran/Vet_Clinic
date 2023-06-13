80da00dcc1de485489b5d2f26702207d246f953c
Oscar Villagran <oscar.villagran@hotmail.com>
2023-06-13 16:29:27 -0500

    Add data.sql update

--- data.sql
+++ data.sql
@@ -5,3 +5,12 @@ VALUES ('Agumon', '02-03-20', 0, TRUE, 10.23),
     ('Gabumon', '11-15-18', 2, TRUE, 8), 
     ('Pikachu', '01-07-21', 1, FALSE, 15.04), 
     ('Devimon', '05-12-17', 5, TRUE, 11);
+
+INSERT INTO animals (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG) 
+VALUES ('Charmander', '02-08-20', 0, false, 11), 
+('Plantmon', '11-15-21', 2, true, 5.7), 
+('Squirtle', '04-02-93', 3, false, 12.13), 
+('Angemon', '06-12-05', 1, true, 45), 
+('Boarmon', '06-07-05', 7, true, 20.4), 
+('Blossom', '10-13-98', 3, true, 17), 
+('Ditto', '05-14-22', 4, true, 22);
